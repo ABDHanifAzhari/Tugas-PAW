582d926c53ffb6ad76de584baf4ef7f2732c3b34
ABDHanifAzhari <han.azha99@gmail.com>
2022-03-12 20:14:11 +0700

    menambah file variabel.php

---
+++ variabel.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = 20;
+$b = 28;
+$c = 16;
+$d = 31;
+
+echo "penjumlahan : "."a"." + "."b"." = ". $a + $b ;
+echo "<br> pengurangan : "."c"." - "."b"." = ". $c - $b;
+echo "<br> perkalian : "."d"." * "."a"." = ". $d * $a;
+
+?>
