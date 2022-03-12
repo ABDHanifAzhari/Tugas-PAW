249a15adad50f83cc1d1389e1f3380676a773acd
ABDHanifAzhari <han.azha99@gmail.com>
2022-03-12 20:14:34 +0700

    menambah file fungsi.php

---
+++ fungsi.php
@@ -0,0 +1,16 @@
+<?php
+
+function identitas($nama,$nim){
+echo "NAMA ----- ".$nama."<br>";
+echo "NIM ----- ". $nim."<hr>";
+}
+identitas("hanif","200411100101");
+
+function ipk($ipklama,$ips)
+{
+echo "ipk (terbaru) "." = ".($ipklama +$ips) / 2;
+echo "<hr>";
+}
+ipk("3.47","3.25");
+
+?>
